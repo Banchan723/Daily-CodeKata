2e470ec3de97010cffe7177a8a1457047337da7c
Banchan723 <parkcy723@naver.com>
2025-01-24 09:58:53 +0900

    [level 1] Title: 직사각형 별찍기, Time: 4.81 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 
 int main() {
-    int n, m;
+    int n;
+    int m;
     cin >> n >> m;
 
-    for (int i = 0; i < m; i++) { // 세로 길이 m만큼 반복
-        for (int j = 0; j < n; j++) { // 가로 길이 n만큼 반복
+    for (int i = 0; i < m; i++) 
+    { 
+        for (int j = 0; j < n; j++) 
+        {
             cout << "*";
         }
-        cout << endl; // 한 줄 끝난 후 줄바꿈
+        cout << endl; 
     }
 
     return 0;
