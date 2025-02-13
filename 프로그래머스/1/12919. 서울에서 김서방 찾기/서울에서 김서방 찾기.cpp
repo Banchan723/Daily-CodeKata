98c1ed201201f885f5e80d194571613dc4317583
Banchan723 <parkcy723@naver.com>
2025-02-13 20:04:08 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -1,22 +1,18 @@
-#include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 string solution(vector<string> seoul) {
-    // "Kim"의 위치를 찾는다
-    for (int i = 0; i < seoul.size(); i++) {
-        if (seoul[i] == "Kim") {
-            // 위치를 기반으로 원하는 문자열을 반환
-            return "김서방은 " + to_string(i) + "에 있다";
+    string answer = "";
+    
+    for (int i = 0; i < seoul.size(); i++)
+    {
+        if (seoul[i] == "Kim")
+        {
+            answer = "김서방은 " + to_string(i) + "에 있다";
+            break;
         }
     }
-    return ""; // 이 코드는 실행되지 않음 ("Kim"은 반드시 포함된다고 했으므로)
-}
-
-int main() {
-    vector<string> seoul = {"Jane", "Kim"};
-    cout << solution(seoul) << endl;
-    return 0;
+    return answer;
 }
