5d0739a27e3e522f4569a3bb59646c877e267d52
Banchan723 <parkcy723@naver.com>
2025-02-14 11:52:40 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -1,9 +1,23 @@
+#include <string>
 #include <cmath>
+#include <vector>
+
+using namespace std;
 
 long long solution(long long n) {
-    long long sqrt_n = sqrt(n);  // 제곱근 구하기
-    if (sqrt_n * sqrt_n == n) {
-        return (sqrt_n + 1) * (sqrt_n + 1);  // x+1의 제곱 반환
+    long long answer = 0;
+    long long x = sqrt(n);
+    
+    if (x * x == n)
+    {
+        answer = (x + 1) * (x + 1);
     }
-    return -1;  // 제곱수가 아니면 -1 반환
+    else
+    {
+        answer = -1;
+    }
+    
+    
+    
+    return answer;
 }
