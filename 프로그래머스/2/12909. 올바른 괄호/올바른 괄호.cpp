3c4f835e5a8d24a9943823244373187c7578b8ab
Banchan723 <parkcy723@naver.com>
2025-07-16 14:29:14 +0900

    [level 2] Title: 올바른 괄호, Time: 0.52 ms, Memory: 3.86 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,48 @@
+#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    
+    stack<char> space;
+    
+    if (s[0] == ')')
+    {
+        answer = false;
+    }
+    else
+    {
+         for (int i = 0; i < s.size(); i++)
+         {
+             if (s[i] == '(')
+             {
+                 space.push('(');
+             }
+             else
+             {
+                 if (!space.empty() && s[i] == ')')
+                 {
+                     space.pop();
+                 }
+             }
+             
+             if (space.empty())
+             {
+                 answer = true;
+             }
+             else
+             {
+                 answer = false;
+             }
+         }
+        
+    }
+    
+   
+    
+    return answer;
+}
